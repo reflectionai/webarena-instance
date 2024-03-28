05d9205addb091ccbabeb565bed036835bb35365
ethanabrooks <ethanabrooks@gmail.com>
2024-03-28 16:21:55 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/";
+    utils.url = "github:numtide/flake-utils/";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }: let
+    out = system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      inherit (pkgs) poetry2nix;
+      overrides = pyfinal: pyprev: rec {};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          alejandra
+          jq
+          postgresql
+        ];
+        PYTHONBREAKPOINT = "ipdb.set_trace";
+        shellHook = ''
+          set -o allexport
+          source .env
+          set +o allexport
+        '';
+      };
+    };
+  in
+    utils.lib.eachDefaultSystem out;
+}
