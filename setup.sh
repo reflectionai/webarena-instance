78fb3cf6fd2ad225ce6cf08738a452646af7bcd3
ethanabrooks <ethanabrooks@gmail.com>
2024-03-28 16:24:28 -0400

    add setup scripts

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker start gitlab
+docker start shopping
+docker start shopping_admin
+docker start forum
+docker start kiwix33
+cd /home/ubuntu/openstreetmap-website/
+docker compose start
+sleep 60 # Wait for services to start
+# Replace <your-server-hostname> with the actual hostname obtained dynamically
+HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+sed -i "s|<your-server-hostname>|${HOSTNAME}|g" host_commands.sh
