8d22700f1eadaccfc560fe71546204a26540a8bc
ethanabrooks <ethanabrooks@gmail.com>
2024-03-28 17:57:33 -0400

    uncomment more stuff in setup.sh

--- setup.sh
+++ setup.sh
@@ -26,16 +26,16 @@ HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://${HOSTNAME}:7770" # no trailing /
 # docker exec shopping mysql -u magentouser -pMyPassword magentodb -e "UPDATE core_config_data SET value='http://${HOSTNAME}:7770/' WHERE path = 'web/secure/base_url';"
 
-# # Remove the requirement to reset password
-# docker exec shopping_admin php /var/www/magento2/bin/magento config:set admin/security/password_is_forced 0
-# docker exec shopping_admin php /var/www/magento2/bin/magento config:set admin/security/password_lifetime 0
-# docker exec shopping /var/www/magento2/bin/magento cache:flush
+# Remove the requirement to reset password
+docker exec shopping_admin php /var/www/magento2/bin/magento config:set admin/security/password_is_forced 0
+docker exec shopping_admin php /var/www/magento2/bin/magento config:set admin/security/password_lifetime 0
+docker exec shopping /var/www/magento2/bin/magento cache:flush
 
-# # Additional Magento admin setup with dynamic hostname
-# docker exec shopping_admin /var/www/magento2/bin/magento setup:store-config:set --base-url="http://${HOSTNAME}:7780"
+# Additional Magento admin setup with dynamic hostname
+docker exec shopping_admin /var/www/magento2/bin/magento setup:store-config:set --base-url="http://${HOSTNAME}:7780"
 # docker exec shopping_admin mysql -u magentouser -pMyPassword magentodb -e "UPDATE core_config_data SET value='http://${HOSTNAME}:7780/' WHERE path = 'web/secure/base_url';"
 # docker exec shopping_admin /var/www/magento2/bin/magento cache:flush
 
-# # GitLab configuration update with dynamic hostname
-# docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
-# docker exec gitlab gitlab-ctl reconfigure
+# GitLab configuration update with dynamic hostname
+docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
+docker exec gitlab gitlab-ctl reconfigure
