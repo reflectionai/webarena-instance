690cba43041fe7830cc550ed65ca4d0f5eecdccf
ethanabrooks <ethanabrooks@gmail.com>
2024-03-28 17:02:31 -0400

    curl setup_host.sh from github

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ cd /home/ubuntu/openstreetmap-website/
 docker compose start
 sleep 60 # Wait for services to start
 # Replace <your-server-hostname> with the actual hostname obtained dynamically
+curl -o setup_host.sh https://raw.githubusercontent.com/reflectionai/webarena-instance/main/setup_host.sh
 HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 sed -i "s|<your-server-hostname>|${HOSTNAME}|g" setup_host.sh
