43b2a81820c8978626e3389212b85d78b42e6502
ethanabrooks <ethanabrooks@gmail.com>
2024-03-29 11:21:55 -0400

    comment out docker exec gitlab commands

--- setup.sh
+++ setup.sh
@@ -26,7 +26,6 @@ echo "Webarena homepage is running on port 4399"
 
 sleep 60
 
-
 # Magento setup with dynamic hostname
 docker exec shopping /var/www/magento2/bin/magento setup:store-config:set --base-url="http://${HOSTNAME}:7770" # no trailing /
 docker exec shopping mysql -u magentouser -pMyPassword magentodb -e "UPDATE core_config_data SET value='http://${HOSTNAME}:7770/' WHERE path = 'web/secure/base_url';"
@@ -42,8 +41,8 @@ docker exec shopping_admin mysql -u magentouser -pMyPassword magentodb -e "UPDAT
 docker exec shopping_admin /var/www/magento2/bin/magento cache:flush
 
 # GitLab configuration update with dynamic hostname
-docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
-docker exec gitlab gitlab-ctl reconfigure
+# docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
+# docker exec gitlab gitlab-ctl reconfigure
 
 touch /home/ubuntu/setup_complete.txt
 echo "Setup complete."
