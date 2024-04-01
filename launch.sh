e19ac419e87e6975ae2fc387fd756691e0a3b04e
ethanabrooks <ethanabrooks@gmail.com>
2024-04-01 10:11:26 -0400

    reduce volume size to 1000

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch the EC2 instance and extract the Instance ID
-INSTANCE_JSON=$(aws ec2 run-instances --image-id ami-06290d70feea35450 --count 1 --instance-type t3a.xlarge --key-name webarena --security-group-ids launch-wizard-1 --block-device-mappings DeviceName=/dev/sda1,Ebs={VolumeSize=1250} --user-data file://setup.sh --region us-east-2)
+INSTANCE_JSON=$(aws ec2 run-instances --image-id ami-06290d70feea35450 --count 1 --instance-type t3a.xlarge --key-name webarena --security-group-ids launch-wizard-1 --block-device-mappings DeviceName=/dev/sda1,Ebs={VolumeSize=1000} --user-data file://setup.sh --region us-east-2)
 INSTANCE_ID=$(echo "$INSTANCE_JSON" | jq -r '.Instances[0].InstanceId')
 echo "Launched EC2 Instance with ID: $INSTANCE_ID"
 
