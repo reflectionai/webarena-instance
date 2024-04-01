1e3a821dd84824e88b6a701ef26d394edf2b57cd
ethanabrooks <ethanabrooks@gmail.com>
2024-04-01 12:32:53 -0400

    remove poetry2nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,31 @@
 {
+  description = "A Nix flake for Python project development with Poetry";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/";
-    utils.url = "github:numtide/flake-utils/";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    utils,
-  }: let
-    out = system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      inherit (pkgs) poetry2nix;
-      overrides = pyfinal: pyprev: rec {};
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          alejandra
-          jq
-          awscli2
-        ];
-        PYTHONBREAKPOINT = "ipdb.set_trace";
-        shellHook = ''
-          set -o allexport
-          source .env
-          set +o allexport
-        '';
-      };
-    };
-  in
-    utils.lib.eachDefaultSystem out;
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+            jq
+            awscli2
+            poetry
+          ];
+          PYTHONBREAKPOINT = "ipdb.set_trace";
+        };
+      }
+    );
 }
