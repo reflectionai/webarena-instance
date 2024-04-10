8a750970327f74306fab32dee758abeafeb6afce
ethanabrooks <ethanabrooks@gmail.com>
2024-04-10 07:58:03 -0400

    update CORS on webarena-instance

--- app/app.py
+++ app/app.py
@@ -15,7 +15,10 @@ app = fastapi.FastAPI()
 
 app.add_middleware(
     cors.CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", "http://127.0.0.1:5173",
+        "http://genesis-frontend.storage.googleapis.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
