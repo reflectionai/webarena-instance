5246995f90b4f5637be0b6da801070a6ec7c21c4
ethanabrooks <ethanabrooks@gmail.com>
2024-04-10 22:17:22 -0400

    comment out all but shopping logic

--- setup.sh
+++ setup.sh
@@ -4,27 +4,27 @@ set -x
 start_time=$(date +%s)
 
 # Start Docker containers
-docker start gitlab
+# docker start gitlab
 docker start shopping
 docker start shopping_admin
-docker start forum
-docker start kiwix33
-cd /home/ubuntu/openstreetmap-website/
-docker compose start
+# docker start forum
+# docker start kiwix33
+# cd /home/ubuntu/openstreetmap-website/
+# docker compose start
 
 # Fetch the instance's public hostname
 HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
 # Wait for services to start
 # Dockerize and run the Flask application
-cd /home/ubuntu/webarena/environment_docker/webarena-homepage
-perl -pi -e "s|<your-server-hostname>|http://${HOSTNAME}|g" /home/ubuntu/webarena/environment_docker/webarena-homepage/templates/index.html
-perl -pi -e "s|http://metis.lti.cs.cmu.edu:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing|http://${HOSTNAME}:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing|g" templates/index.html
+# cd /home/ubuntu/webarena/environment_docker/webarena-homepage
+# perl -pi -e "s|<your-server-hostname>|http://${HOSTNAME}|g" /home/ubuntu/webarena/environment_docker/webarena-homepage/templates/index.html
+# perl -pi -e "s|http://metis.lti.cs.cmu.edu:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing|http://${HOSTNAME}:8888/wikipedia_en_all_maxi_2022-05/A/User:The_other_Kiwix_guy/Landing|g" templates/index.html
 
-curl -o Dockerfile https://raw.githubusercontent.com/reflectionai/webarena-instance/main/Dockerfile
-docker build -t webarena-homepage .
-docker run -d -p 4399:4399 --name webarena-homepage webarena-homepage
-echo "Webarena homepage is running on port 4399"
+# curl -o Dockerfile https://raw.githubusercontent.com/reflectionai/webarena-instance/main/Dockerfile
+# docker build -t webarena-homepage .
+# docker run -d -p 4399:4399 --name webarena-homepage webarena-homepage
+# echo "Webarena homepage is running on port 4399"
 
 mkdir /home/ubuntu/app
 cd /home/ubuntu/app
@@ -58,43 +58,43 @@ commit_start_time=$(date +%s)
 
 docker commit shopping snapshot-shopping:initial
 docker commit shopping_admin snapshot-shopping_admin:initial
-docker commit forum snapshot-forum:initial
+# docker commit forum snapshot-forum:initial
 # ommitting kiwix33 because stateless
 
 end_time=$(date +%s)
-total_time=$((end_time-commit_start_time))
-echo "All containers committed, except for gitlab. Total duration: $total_time seconds."
-echo "Now waiting for gitlab to finish initializing..."
+# total_time=$((end_time-commit_start_time))
+# echo "All containers committed, except for gitlab. Total duration: $total_time seconds."
+# echo "Now waiting for gitlab to finish initializing..."
 
-CONTAINER_NAME="gitlab"
+# CONTAINER_NAME="gitlab"
 
-wait_start_time=$(date +%s)
+# wait_start_time=$(date +%s)
 
-# Loop until the container's health status is "healthy"
-while true; do
-    # Get the current health status of the container
-    HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME")
-    echo "$CONTAINER_NAME is $HEALTH_STATUS."
+# # Loop until the container's health status is "healthy"
+# while true; do
+#     # Get the current health status of the container
+#     HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME")
+#     echo "$CONTAINER_NAME is $HEALTH_STATUS."
 
-    # Check if the health status is "healthy"
-    if [ "$HEALTH_STATUS" == "healthy" ]; then
-        break
-    fi
+#     # Check if the health status is "healthy"
+#     if [ "$HEALTH_STATUS" == "healthy" ]; then
+#         break
+#     fi
 
-    # Calculate and echo the wait duration each loop iteration
-    wait_time=$(date +%s)
-    wait_duration=$((wait_time-wait_start_time))
-    echo "Waited $wait_duration seconds for $CONTAINER_NAME to become healthy."
-    sleep 10
-done
+#     # Calculate and echo the wait duration each loop iteration
+#     wait_time=$(date +%s)
+#     wait_duration=$((wait_time-wait_start_time))
+#     echo "Waited $wait_duration seconds for $CONTAINER_NAME to become healthy."
+#     sleep 10
+# done
 
-# GitLab configuration update with dynamic hostname
-docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
-docker exec gitlab gitlab-ctl reconfigure
-docker commit gitlab snapshot-gitlab:initial
+# # GitLab configuration update with dynamic hostname
+# docker exec gitlab sed -i "s|^external_url.*|external_url 'http://${HOSTNAME}:8023'|" /etc/gitlab/gitlab.rb
+# docker exec gitlab gitlab-ctl reconfigure
+# docker commit gitlab snapshot-gitlab:initial
 
-end_time=$(date +%s)
-total_time=$((end_time-wait_start_time))
-echo "Gitlab initialized. Total duration: $total_time seconds."
+# end_time=$(date +%s)
+# total_time=$((end_time-wait_start_time))
+# echo "Gitlab initialized. Total duration: $total_time seconds."
 total_time=$((end_time-start_time))
 echo "Setup complete. Total duration: $total_time seconds."
